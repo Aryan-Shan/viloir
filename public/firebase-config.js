abb4c664c081998ec009695b72db3ff44c3114df
Aryan-Shan <aryanshandilyajvm@gmail.com>
2024-09-21 14:59:31 +0530

    minor changes

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,12 +1,13 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyDkg90BY6ioCqh7dM3KJnfwWq_xqeGRw6A",
+    authDomain: "viloir.firebaseapp.com",
+    projectId: "viloir",
+    storageBucket: "viloir.appspot.com",
+    messagingSenderId: "889475158742",
+    appId: "1:889475158742:web:4e336e477677b9788e2f77",
+    measurementId: "G-JN37G30V5Q"
+  };
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
