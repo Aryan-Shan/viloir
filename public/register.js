53b5af26bbd03752f552817110ba3f8f13e01827
Aryan-Shan <aryanshandilyajvm@gmail.com>
2024-09-21 21:21:36 +0530

    minor changes

---
+++ public/register.js
@@ -0,0 +1,22 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDkg90BY6ioCqh7dM3KJnfwWq_xqeGRw6A",
+    authDomain: "viloir.firebaseapp.com",
+    projectId: "viloir",
+    storageBucket: "viloir.appspot.com",
+    messagingSenderId: "889475158742",
+    appId: "1:889475158742:web:5d54324e3d0696048e2f77",
+    measurementId: "G-FQF7WW4LZK"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
